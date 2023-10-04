428862a3858f3b629bba9f54b35cd860870b3454
mausam-office <eng3@ramlaxmangroup.com>
2023-10-04 15:38:23 +0545

    removed logging recognition results

--- main.py
+++ main.py
@@ -383,7 +383,6 @@ def logging_removing(results, filepath):
     # perform database operation
     # print('results ', results)
     if results:
-        debug_error_log(f'results: {str(results)}')
         for result in list(results.values()):
             # print('single result ', result)
             channel_id = get_channel_id(filepath)
